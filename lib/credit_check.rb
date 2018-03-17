50d7f3de712fba707e5e978f24492b2cb96a816b
justinetroyke <jjtroyke@gmail.com>
2018-03-17 14:14:35 -0600

    up to double method which multiplies every other digit

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -7,7 +7,21 @@ class CreditCheck
     @card_number = card_number
   end
 
-#   def convert_to_array
-#     @card_number.split
-#   end
+  def convert_to_array
+    reversed_array = @card_number.to_i.digits
+  end
+
+  def double
+    doubled = convert_to_array.map.with_index do |number, index|
+    number * 2 if index.odd?
+    end
+    doubled.compact
+  end
+
 end
+
+# def checker
+  # multiply every other
+  # add digits over 10 togerther
+  # sum results
+# end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -18,4 +18,22 @@ class CreditCheckTest < Minitest::Test
     assert_equal "5541808923795240", check.card_number
   end
 
+  def test_card_number_is_a_reversed_array
+   check = CreditCheck.new("5541808923795240")
+
+    assert_equal [0,4,2,5,9,7,3,2,9,8,0,8,1,4,5,5], check.convert_to_array
+  end
+
+  def test_every_other_digit_times_2
+    check = CreditCheck.new("5541808923795240")
+
+
+    assert_equal [8,10,14,4,16,16,8,10], check.double
+  end
+
+  def test_digits_over_10_get_added
+    skip
+
+  end
+
 end
